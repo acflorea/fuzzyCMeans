f0f00a186a52e1d886bf14dec2aa2dfd9028f8c3
acflorea <acflorea@gmail.com>
2016-04-09 14:54:00 +0300

    Add ml (Dataframe) API and tests

---
+++ src/main/scala/org/apache/spark/ml/clustering/FuzzyCMeans.scala
@@ -0,0 +1,196 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.ml.clustering
+
+import org.apache.spark.annotation.{Experimental, Since}
+import org.apache.spark.ml.param._
+import org.apache.spark.ml.param.shared._
+import org.apache.spark.ml.util.{Identifiable, SchemaUtils}
+import org.apache.spark.ml.{Estimator, Model}
+import org.apache.spark.mllib.clustering.{FuzzyCMeans => MLlibFuzzyCMeans, FuzzyCMeansModel => MLlibFuzzyCMeansModel}
+import org.apache.spark.mllib.linalg.{Vector, VectorUDT}
+import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.types.{IntegerType, StructType}
+import org.apache.spark.sql.{DataFrame, Row}
+
+
+/**
+ * Common params for KMeans and KMeansModel
+ */
+private[clustering] trait FuzzyCMeansParams extends Params with HasMaxIter with HasFeaturesCol
+  with HasSeed with HasPredictionCol with HasTol {
+
+  /**
+   * Set the number of clusters to create (k). Must be > 1. Default: 2.
+   *
+   * @group param
+   */
+  final val k = new IntParam(this, "k", "number of clusters to create", (x: Int) => x > 1)
+
+  /** @group getParam */
+  def getK: Int = $(k)
+
+  /**
+   * Set the number of clusters to create (k). Must be > 1. Default: 2.
+   *
+   * @group param
+   */
+  final val m = new DoubleParam(this, "m", "fuzzyfier factor", (x: Double) => x > 1)
+
+  /** @group getParam */
+  def getM: Double = $(m)
+
+  /**
+   * Param for the initialization algorithm. This can be either "random" to choose random points as
+   * initial cluster centers, or "k-means||" to use a parallel variant of k-means++
+   * (Bahmani et al., Scalable K-Means++, VLDB 2012). Default: k-means||.
+   *
+   * @group expertParam
+   */
+  final val initMode = new Param[String](this, "initMode", "initialization algorithm",
+    (value: String) => MLlibFuzzyCMeans.validateInitMode(value))
+
+  /** @group expertGetParam */
+  def getInitMode: String = $(initMode)
+
+  /**
+   * Param for the number of steps for the k-means|| initialization mode. This is an advanced
+   * setting -- the default of 5 is almost always enough. Must be > 0. Default: 5.
+   *
+   * @group expertParam
+   */
+  final val initSteps = new IntParam(this, "initSteps", "number of steps for k-means||",
+    (value: Int) => value > 0)
+
+  /** @group expertGetParam */
+  def getInitSteps: Int = $(initSteps)
+
+  /**
+   * Validates and transforms the input schema.
+   *
+   * @param schema input schema
+   * @return output schema
+   */
+  protected def validateAndTransformSchema(schema: StructType): StructType = {
+    SchemaUtils.checkColumnType(schema, $(featuresCol), new VectorUDT)
+    SchemaUtils.appendColumn(schema, $(predictionCol), IntegerType)
+  }
+}
+
+/**
+ * :: Experimental ::
+ * Model fitted by KMeans.
+ *
+ * @param parentModel a model trained by spark.mllib.clustering.KMeans.
+ */
+class FuzzyCMeansModel private[ml](
+                                    override val uid: String,
+                                    private val parentModel: MLlibFuzzyCMeansModel) extends Model[FuzzyCMeansModel] with FuzzyCMeansParams {
+
+  override def copy(extra: ParamMap): FuzzyCMeansModel = {
+    val copied = new FuzzyCMeansModel(uid, parentModel)
+    copyValues(copied, extra)
+  }
+
+  override def transform(dataset: DataFrame): DataFrame = {
+    val predictUDF = udf((vector: Vector) => fuzzyPredict(vector))
+    dataset.withColumn($(predictionCol), predictUDF(col($(featuresCol))))
+  }
+
+  override def transformSchema(schema: StructType): StructType = {
+    validateAndTransformSchema(schema)
+  }
+
+  private[clustering] def predict(features: Vector): Int = parentModel.predict(features)
+
+  private[clustering] def fuzzyPredict(features: Vector): Seq[(Int, Double)] = parentModel.fuzzyPredict(features)
+
+  def clusterCenters: Array[Vector] = parentModel.clusterCenters
+
+}
+
+/**
+ *
+ * K-means clustering with support for k-means|| initialization proposed by Bahmani et al.
+ *
+ * @see [[http://dx.doi.org/10.14778/2180912.2180915 Bahmani et al., Scalable k-means++.]]
+ */
+class FuzzyCMeans(
+                   override val uid: String)
+  extends Estimator[FuzzyCMeansModel] with FuzzyCMeansParams {
+
+  setDefault(
+    k -> 2,
+    m -> 2,
+    maxIter -> 20,
+    initMode -> MLlibFuzzyCMeans.K_MEANS_PARALLEL,
+    initSteps -> 5,
+    tol -> 1e-4)
+
+  override def copy(extra: ParamMap): FuzzyCMeans = defaultCopy(extra)
+
+  def this() = this(Identifiable.randomUID("fuzzycmeans"))
+
+  /** @group setParam */
+  def setFeaturesCol(value: String): this.type = set(featuresCol, value)
+
+  /** @group setParam */
+  def setPredictionCol(value: String): this.type = set(predictionCol, value)
+
+  /** @group setParam */
+  def setK(value: Int): this.type = set(k, value)
+
+  /** @group setParam */
+  def setM(value: Double): this.type = set(m, value)
+
+  /** @group expertSetParam */
+  def setInitMode(value: String): this.type = set(initMode, value)
+
+  /** @group expertSetParam */
+  def setInitSteps(value: Int): this.type = set(initSteps, value)
+
+  /** @group setParam */
+  def setMaxIter(value: Int): this.type = set(maxIter, value)
+
+  /** @group setParam */
+  def setTol(value: Double): this.type = set(tol, value)
+
+  /** @group setParam */
+  def setSeed(value: Long): this.type = set(seed, value)
+
+  override def fit(dataset: DataFrame): FuzzyCMeansModel = {
+    val rdd = dataset.select(col($(featuresCol))).map { case Row(point: Vector) => point }
+
+    val algo = new MLlibFuzzyCMeans()
+      .setK($(k))
+      .setM($(m))
+      .setInitializationMode($(initMode))
+      .setInitializationSteps($(initSteps))
+      .setMaxIterations($(maxIter))
+      .setSeed($(seed))
+      .setEpsilon($(tol))
+    val parentModel = algo.run(rdd)
+    val model = new FuzzyCMeansModel(uid, parentModel)
+    copyValues(model)
+  }
+
+  override def transformSchema(schema: StructType): StructType = {
+    validateAndTransformSchema(schema)
+  }
+}
+
---
+++ src/test/scala/org/apache/spark/ml/clustering/FuzzyCMeansSuite.scala
@@ -0,0 +1,118 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.ml.clustering
+
+import org.apache.spark.SparkFunSuite
+import org.apache.spark.mllib.clustering.{FuzzyCMeans => MLlibFuzzyCMeans}
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.util.MLlibTestSparkContext
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
+
+private case class TestRow(features: Vector)
+
+object FuzzyCMeansSuite {
+  def generateFuzzyCMeansData(sql: SQLContext, rows: Int, dim: Int, k: Int): DataFrame = {
+    val sc = sql.sparkContext
+    val rdd = sc.parallelize(1 to rows).map(i => Vectors.dense(Array.fill(dim)((i % k).toDouble)))
+      .map(v => new TestRow(v))
+    sql.createDataFrame(rdd)
+  }
+}
+
+class FuzzyCMeansSuite extends SparkFunSuite with MLlibTestSparkContext {
+
+  final val k = 5
+  @transient var dataset: DataFrame = _
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+
+    dataset = FuzzyCMeansSuite.generateFuzzyCMeansData(sqlContext, 50, 3, k)
+  }
+
+  test("default parameters") {
+    val fuzzyCmeans = new FuzzyCMeans()
+
+    assert(fuzzyCmeans.getK === 2)
+    assert(fuzzyCmeans.getM === 2.0)
+    assert(fuzzyCmeans.getFeaturesCol === "features")
+    assert(fuzzyCmeans.getPredictionCol === "prediction")
+    assert(fuzzyCmeans.getMaxIter === 20)
+    assert(fuzzyCmeans.getInitMode === MLlibFuzzyCMeans.K_MEANS_PARALLEL)
+    assert(fuzzyCmeans.getInitSteps === 5)
+    assert(fuzzyCmeans.getTol === 1e-4)
+  }
+
+  test("set parameters") {
+    val fuzzyCmeans = new FuzzyCMeans()
+      .setK(9)
+      .setM(1.5)
+      .setFeaturesCol("test_feature")
+      .setPredictionCol("test_prediction")
+      .setMaxIter(33)
+      .setInitMode(MLlibFuzzyCMeans.RANDOM)
+      .setInitSteps(3)
+      .setSeed(123)
+      .setTol(1e-3)
+
+    assert(fuzzyCmeans.getK === 9)
+    assert(fuzzyCmeans.getM === 1.5)
+    assert(fuzzyCmeans.getFeaturesCol === "test_feature")
+    assert(fuzzyCmeans.getPredictionCol === "test_prediction")
+    assert(fuzzyCmeans.getMaxIter === 33)
+    assert(fuzzyCmeans.getInitMode === MLlibFuzzyCMeans.RANDOM)
+    assert(fuzzyCmeans.getInitSteps === 3)
+    assert(fuzzyCmeans.getSeed === 123)
+    assert(fuzzyCmeans.getTol === 1e-3)
+  }
+
+  test("parameters validation") {
+    intercept[IllegalArgumentException] {
+      new FuzzyCMeans().setK(1)
+    }
+    intercept[IllegalArgumentException] {
+      new FuzzyCMeans().setInitMode("no_such_a_mode")
+    }
+    intercept[IllegalArgumentException] {
+      new FuzzyCMeans().setInitSteps(0)
+    }
+    intercept[IllegalArgumentException] {
+      new FuzzyCMeans().setM(0.9)
+    }
+  }
+
+  test("fit & transform") {
+    val predictionColName = "fuzzymeans_prediction"
+    val cMeans = new FuzzyCMeans().setK(k).setPredictionCol(predictionColName).setSeed(1)
+    val cMeansModel = cMeans.fit(dataset)
+    assert(cMeansModel.clusterCenters.length === k)
+
+    val transformed = cMeansModel.transform(dataset)
+    val expectedColumns = Array("features", predictionColName)
+    expectedColumns.foreach { column =>
+      assert(transformed.columns.contains(column))
+    }
+    val clusters = transformed.select(predictionColName).map(_.getSeq[Row](0)).distinct().collect().toSet
+    assert(clusters.size === k)
+
+    val hardPredictions = clusters.map(cluster => cluster.maxBy(_.getDouble(1))).map(_.getInt(0))
+
+    assert(hardPredictions === Set(0, 1, 2, 3, 4))
+
+  }
+}
